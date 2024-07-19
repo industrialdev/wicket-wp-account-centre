113d1c261aefa52beb4b2df4ae6476860682578a
Esteban <esteban@attitude.cl>
2024-07-19 19:37:03 -0400

    AC Profile Picture
    https://app.asana.com/0/1204462747587391/1207762036297835/f

--- class-wicket-acc-main.php
+++ class-wicket-acc-main.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * @package  wicket-account-centre
@@ -17,58 +18,59 @@
  *
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
-if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ) ) {
+if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')), true)) {
 	/**
 	 * Show Required Plugin Notice
 	 */
-	function wicket_acc_admin_notice() {
+	function wicket_acc_admin_notice()
+	{
 
 		// Deactivate the plugin.
-		deactivate_plugins( __FILE__ );
+		deactivate_plugins(__FILE__);
 
 		$wicket_acc_plugin_check = '<div id="message" class="error">
             <p><strong>Wicket Account Centre plugin is inactive.</strong> The <a href="http://wordpress.org/extend/plugins/woocommerce/">WooCommerce plugin</a> must be active for this plugin to be used. Please install &amp; activate WooCommerce »</p></div>';
-		echo wp_kses_post( $wicket_acc_plugin_check );
-
+		echo wp_kses_post($wicket_acc_plugin_check);
 	}
 
-	add_action( 'admin_notices', 'wicket_acc_admin_notice' );
+	add_action('admin_notices', 'wicket_acc_admin_notice');
 }
 
-if ( ! class_exists( 'Wicket_Acc_Main' ) ) {
+if (!class_exists('Wicket_Acc_Main')) {
 	/**
 	 * The main Wicket Account Centre class
 	 */
-	class Wicket_Acc_Main {
+	class Wicket_Acc_Main
+	{
 		/**
 		 * Constructor
 		 */
-		public function __construct() {
+		public function __construct()
+		{
 
 			// Define global constants
 			$this->wicket_acc_global_constants_vars();
 
 			// Load text domain
-			add_action( 'wp_loaded', array( $this, 'wicket_acc_init' ) );
+			add_action('wp_loaded', array($this, 'wicket_acc_init'));
 			// Create custom post type
-			add_action( 'init', array( $this, 'wicket_acc_register_custom_post_type' ) );
+			add_action('init', array($this, 'wicket_acc_register_custom_post_type'));
 			// Add menu locations
-			add_action( 'after_setup_theme', array( $this, 'wicket_acc_custom_nav_menus' ) );
+			add_action('after_setup_theme', array($this, 'wicket_acc_custom_nav_menus'));
 			// Add Wicket Widgets Icons
-			add_action( 'wp_enqueue_scripts', array( $this, 'wicket_acc_styles' ) );
+			add_action('wp_enqueue_scripts', array($this, 'wicket_acc_styles'));
 
 			// Registration hook setting
-			register_activation_hook( __FILE__, array( $this, 'wicket_acc_install_settings' ) );
+			register_activation_hook(__FILE__, array($this, 'wicket_acc_install_settings'));
 			// Include other files
-			if ( is_admin() ) {
+			if (is_admin()) {
 				// include admin class
 				include_once WICKET_ACC_PLUGIN_DIR . 'admin/class-wicket-acc-admin.php';
 				include_once WICKET_ACC_PLUGIN_DIR . 'front/class-wicket-acc-front.php';
-
 			} else {
 				// include front class
 				include_once WICKET_ACC_PLUGIN_DIR . 'front/class-wicket-acc-front.php';
@@ -83,73 +85,82 @@ if ( ! class_exists( 'Wicket_Acc_Main' ) ) {
 			//HOPS compatibility
 			add_action('before_woocommerce_init', array($this, 'wicket_acc_HPOS_Compatibility'));
 
-                        add_filter( 'wp_dropdown_pages', 'wicket_acc_alter_wp_job_manager_pages', 10, 3 );
-                        add_filter('post_type_link', 'wicket_acc_rewrite_permalinks', 50, 4);
+			add_filter('wp_dropdown_pages', 'wicket_acc_alter_wp_job_manager_pages', 10, 3);
+			add_filter('post_type_link', 'wicket_acc_rewrite_permalinks', 50, 4);
 		}
 
-		public function wicket_acc_HPOS_Compatibility() {
+		public function wicket_acc_HPOS_Compatibility()
+		{
 
-			if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
-				\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+			if (class_exists(\Automattic\WooCommerce\Utilities\FeaturesUtil::class)) {
+				\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility('custom_order_tables', __FILE__, true);
 			}
 		}
 
 		/**
 		 * Define Global variables function
 		 */
-		public function wicket_acc_global_constants_vars() {
+		public function wicket_acc_global_constants_vars()
+		{
 
-			if ( ! defined( 'WICKET_ACC_URL' ) ) {
-				define( 'WICKET_ACC_URL', plugin_dir_url( __FILE__ ) );
+			if (!defined('WICKET_ACC_URL')) {
+				define('WICKET_ACC_URL', plugin_dir_url(__FILE__));
 			}
 
-			if ( ! defined( 'WICKET_ACC_BASENAME' ) ) {
-				define( 'WICKET_ACC_BASENAME', plugin_basename( __FILE__ ) );
+			if (!defined('WICKET_ACC_BASENAME')) {
+				define('WICKET_ACC_BASENAME', plugin_basename(__FILE__));
 			}
 
-			if ( ! defined( 'WICKET_ACC_PLUGIN_DIR' ) ) {
-				define( 'WICKET_ACC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+			if (!defined('WICKET_ACC_PLUGIN_DIR')) {
+				define('WICKET_ACC_PLUGIN_DIR', plugin_dir_path(__FILE__));
+			}
+
+			if (!defined('WICKET_ACC_PLUGIN_URL')) {
+				define('WICKET_ACC_PLUGIN_URL', plugin_dir_url(__FILE__));
 			}
 		}
 
 		/**
 		 * Plugin settings
 		 */
-		public function wicket_acc_install_settings() {
+		public function wicket_acc_install_settings()
+		{
 			// Default setting for plugin.
 		}
 
 		/**
 		 * Load text domain
 		 */
-		public function wicket_acc_init() {
-			if ( function_exists( 'load_plugin_textdomain' ) ) {
-				load_plugin_textdomain( 'wicket-acc', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+		public function wicket_acc_init()
+		{
+			if (function_exists('load_plugin_textdomain')) {
+				load_plugin_textdomain('wicket-acc', false, dirname(plugin_basename(__FILE__)) . '/languages/');
 			}
 		}
 
 		/**
 		 * Register custom post type
 		 */
-		public function wicket_acc_register_custom_post_type() {
+		public function wicket_acc_register_custom_post_type()
+		{
 
 			// Set UI labels for custom post type
 			$labels = array(
 
-				'name'               => esc_html__( 'Account Centre', 'wicket-acc' ),
-				'singular_name'      => esc_html__( 'Page', 'wicket-acc' ),
-				'add_new_item'       => esc_html__( 'Add New Page', 'wicket-acc' ),
-				'add_new'            => esc_html__( 'Add New Page', 'wicket-acc' ),
-				'edit_item'          => esc_html__( 'Edit Page', 'wicket-acc' ),
-				'view_item'          => esc_html__( 'View Page', 'wicket-acc' ),
-				'update_item'        => esc_html__( 'Update Page', 'wicket-acc' ),
-				'search_items'       => esc_html__( 'Search Page', 'wicket-acc' ),
-				'not_found'          => esc_html__( 'Not Found', 'wicket-acc' ),
-				'not_found_in_trash' => esc_html__( 'Not found in Trash', 'wicket-acc' ),
-				'menu_name'          => esc_html__( 'Account Centre', 'wicket-acc' ),
+				'name'               => esc_html__('Account Centre', 'wicket-acc'),
+				'singular_name'      => esc_html__('Page', 'wicket-acc'),
+				'add_new_item'       => esc_html__('Add New Page', 'wicket-acc'),
+				'add_new'            => esc_html__('Add New Page', 'wicket-acc'),
+				'edit_item'          => esc_html__('Edit Page', 'wicket-acc'),
+				'view_item'          => esc_html__('View Page', 'wicket-acc'),
+				'update_item'        => esc_html__('Update Page', 'wicket-acc'),
+				'search_items'       => esc_html__('Search Page', 'wicket-acc'),
+				'not_found'          => esc_html__('Not Found', 'wicket-acc'),
+				'not_found_in_trash' => esc_html__('Not found in Trash', 'wicket-acc'),
+				'menu_name'          => esc_html__('Account Centre', 'wicket-acc'),
 				'parent_item_colon'  => '',
-				'all_items'          => esc_html__( 'All Pages', 'wicket-acc' ),
-				'attributes'         => __( 'Pages Sorting Order' ),
+				'all_items'          => esc_html__('All Pages', 'wicket-acc'),
+				'attributes'         => __('Pages Sorting Order'),
 			);
 
 			// Set other options for custom post type
@@ -167,40 +178,40 @@ if ( ! class_exists( 'Wicket_Acc_Main' ) ) {
 				'has_archive'         => false,
 				'hierarchical'        => false,
 				'menu_position'       => 30,
-				'menu_icon'           => plugins_url( '/assets/img/wicket-icon.png', __FILE__ ),
+				'menu_icon'           => plugins_url('/assets/img/wicket-icon.png', __FILE__),
 				'rewrite'             => array(
 					'slug'            => 'wicket_acc',
 					'with_front'      => false,
 				),
-				'supports'            => array( 'title', 'page-attributes', 'editor' ),
+				'supports'            => array('title', 'page-attributes', 'editor'),
 				'show_in_rest'        => true,
 			);
 			// Registering your Custom Post Type.
-			register_post_type( 'wicket_acc', $args );
+			register_post_type('wicket_acc', $args);
 		}
 
 		/**
 		 * Register menu locations
 		 */
-		public function wicket_acc_custom_nav_menus() {
+		public function wicket_acc_custom_nav_menus()
+		{
 
 			// This theme uses wp_nav_menu() in one location.
-			register_nav_menus( array(
-	      'wicket-acc-nav' => esc_html__( 'Account Centre Menu', 'wicket-acc' ),
-			) );
+			register_nav_menus(array(
+				'wicket-acc-nav' => esc_html__('Account Centre Menu', 'wicket-acc'),
+			));
 
-      // This theme offers a secondary wp_nav_menu()
-			register_nav_menus( array(
-	      'wicket-acc-nav-two' => esc_html__( 'Account Centre Secondary Menu', 'wicket-acc' ),
-			) );
-
-		}
-		
-		public function wicket_acc_styles() {
-			wp_enqueue_style( 'wicket-widgets-icons', "https://fonts.googleapis.com/icon?family=Material+Icons" );
-			wp_enqueue_style( 'wicket-widgets-datepicker', plugins_url('/assets/css/react-datepicker.css', __FILE__ ));
+			// This theme offers a secondary wp_nav_menu()
+			register_nav_menus(array(
+				'wicket-acc-nav-two' => esc_html__('Account Centre Secondary Menu', 'wicket-acc'),
+			));
 		}
 
+		public function wicket_acc_styles()
+		{
+			wp_enqueue_style('wicket-widgets-icons', "https://fonts.googleapis.com/icon?family=Material+Icons");
+			wp_enqueue_style('wicket-widgets-datepicker', plugins_url('/assets/css/react-datepicker.css', __FILE__));
+		}
 	} // end Class Wicket_Acc_Main_Class.
 	new Wicket_Acc_Main();
 }
--- includes/blocks/ac-profile-picture/init.php
+++ includes/blocks/ac-profile-picture/init.php
@@ -10,82 +10,69 @@ namespace Wicket_AC\Blocks\AC_Profile_Picture_Block;
 if (!class_exists('Wicket_Acc_Profile_Picture')) {
 	class Wicket_Acc_Profile_Picture
 	{
-		protected $pp_enabled;
-		protected $pp_max_size;
-		protected $pp_uploads_path;
-		protected $pp_uploads_url;
-		protected $pp_uploads_subdir;
-
 		/**
 		 * Constructor
 		 */
-		public function __construct()
-		{
-			$uploads_dir = wp_get_upload_dir();
-
-			$this->pp_enabled      = get_field('profile_picture_enabled');
-			$this->pp_max_size     = get_field('profile_picture_max_size');
-			$this->pp_uploads_path = $uploads_dir['basedir'] . '/wicket-profile-pictures';
-			$this->pp_uploads_url  = $uploads_dir['baseurl'] . '/wicket-profile-pictures';
+		public function __construct(
+			protected int $pp_max_size = 0,
+			protected string $pp_uploads_path = '',
+			protected string $pp_uploads_url = '',
+			protected string $pp_uploads_subdir = '',
+			protected array $uploads_dir = [],
+			protected array $pp_extensions = []
+		) {
+			$this->uploads_dir       = wp_get_upload_dir();
+			$this->pp_max_size       = absint(get_field('profile_picture_max_size'));         // in MB
+			$this->pp_uploads_path   = $this->uploads_dir['basedir'] . '/wicket-profile-pictures';
+			$this->pp_uploads_url    = $this->uploads_dir['baseurl'] . '/wicket-profile-pictures';
 			$this->pp_uploads_subdir = 'wicket-profile-pictures';
+			$this->pp_extensions     = ['jpg', 'jpeg', 'png', 'gif'];
 
 			$this->display_block();
-
-			// Hook to process the form when the block is displayed
-			add_action('init', [$this, 'process_form']);
 		}
 
 		protected function display_block()
 		{
-			if ($this->pp_enabled) {
-				$pp_profile_picture = $this->get_profile_picture();
-			}
+			$this->process_form();
+
+			$pp_profile_picture = $this->get_profile_picture();
 ?>
-			<div class="wicket-ac-profile-picture">
-				<?php if ($this->pp_enabled) : ?>
-					<div class="max-w-md mx-auto bg-white p-6 rounded-lg">
-						<h2 class="text-2xl font-bold mb-4">
-							<?php _e('Profile Image', 'wicket-acc'); ?>
-						</h2>
-						<div class="flex flex-col items-center">
-							<img src="<?php echo $pp_profile_picture; ?>" alt="<?php _e('Profile Image', 'wicket-acc'); ?>" class="w-24 h-24 rounded-full mb-4 wicket-ac-profile-picture-image">
-							<form name="wicket-ac-profile-picture-form" class="w-full" action="<?php echo esc_url(get_permalink()); ?>" method="post">
-								<label for="profile-image" class="sr-only">
-									<?php _e('Choose File', 'wicket-acc'); ?>
-								</label>
-								<input type="file" id="profile-image" name="profile-image" class="sr-only">
-								<div class="flex justify-between">
-									<label for="profile-image" class="flex items-center px-4 py-2 bg-gray-200 text-gray-700 rounded-md cursor-pointer hover:bg-gray-300">
-										<?php _e('Choose File', 'wicket-acc'); ?>
-									</label>
-									<button type="submit" class="flex items-center px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:ring-2 focus:ring-blue-300 focus:outline-none">
-										<?php _e('Update Image', 'wicket-acc'); ?>
-									</button>
-								</div>
-								<input type="hidden" name="nonce" value="<?php echo wp_create_nonce('wicket-ac-profile-picture-form'); ?>">
-								<input type="hidden" name="user_id" value="<?php echo get_current_user_id(); ?>">
-								<input type="hidden" name="action" value="wicket-ac-profile-picture-form">
-							</form>
-						</div>
+
+			<section class="container wicket-ac-profile-picture">
+				<h2>
+					<?php esc_html_e('Profile Image', 'wicket-acc'); ?>
+				</h2>
+				<div class="profile-image">
+					<img src="<?php echo $pp_profile_picture; ?>" alt="<?php esc_html_e('Profile Image', 'wicket-acc'); ?>" class="profile-image-img">
+				</div>
+				<form name="wicket-ac-profile-picture-form" method="post" enctype="multipart/form-data">
+					<label for="profile-image" class="sr-only">
+						<?php esc_html_e('Choose File', 'wicket-acc'); ?>
+					</label>
+					<input type="file" id="profile-image" name="profile-image" class="sr-only">
+					<div class="buttons">
+						<label for="profile-image" class="btn choose-file">
+							<?php esc_html_e('Choose File', 'wicket-acc'); ?>
+						</label>
+						<button type="submit" class="btn update-image">
+							<?php esc_html_e('Update Image', 'wicket-acc'); ?>
+						</button>
 					</div>
-				<?php else : ?>
-					<div class="max-w-md mx-auto bg-white p-6 rounded-lg">
-						<p><?php _e('Profile pictures are not enabled', 'wicket-acc'); ?></p>
-					</div>
-				<?php endif; ?>
-			</div>
+					<?php wp_nonce_field('wicket-ac-profile-picture-form', 'nonce'); ?>
+					<input type="hidden" name="user_id" value="<?php echo get_current_user_id(); ?>">
+					<input type="hidden" name="action" value="wicket-ac-profile-picture-form">
+				</form>
+			</section>
 <?php
 		}
 
 		/**
-		 * Get the profile picture
+		 * Get the profile picture URL
 		 *
 		 * @return string|bool Profile picture URL, default one or false on error
 		 */
 		protected function get_profile_picture()
 		{
-			global $uploads_dir;
-
 			// Get current WP user ID
 			$current_user_id = get_current_user_id();
 
@@ -95,21 +82,26 @@ if (!class_exists('Wicket_Acc_Profile_Picture')) {
 			}
 
 			// Check for jpg or png
-			$extensions         = ['jpg', 'png'];
-			$pp_profile_picture = null;
+			$extensions = $this->pp_extensions;
+			$pp_profile_picture = '';
+			$pp_valid_extension = '';
 
 			foreach ($extensions as $ext) {
-				$file_path = $uploads_dir['basedir'] . '/wicket-profile-pictures/' . $current_user_id . '.' . $ext;
+				$file_path = $this->uploads_dir['basedir'] . '/wicket-profile-pictures/' . $current_user_id . '.' . $ext;
 				if (file_exists($file_path)) {
 					// Found it!
 					$pp_profile_picture = $file_path;
+					$pp_valid_extension = $ext;
 					break;
 				}
 			}
 
+			// Get file URL
+			$pp_profile_picture = $this->uploads_dir['baseurl'] . '/wicket-profile-pictures/' . $current_user_id . '.' . $pp_valid_extension;
+
 			// Still no image? Return the default svg
-			if (is_null($pp_profile_picture)) {
-				$pp_profile_picture = WICKET_ACC_PLUGIN_DIR . '/assets/img/profile-picture-default.svg';
+			if (empty($pp_profile_picture)) {
+				$pp_profile_picture = WICKET_ACC_PLUGIN_URL . '/assets/img/profile-picture-default.svg';
 			}
 
 			return $pp_profile_picture;
@@ -118,99 +110,79 @@ if (!class_exists('Wicket_Acc_Profile_Picture')) {
 		/**
 		 * Process the form and save the profile picture
 		 *
-		 * @param array $form
-		 *
 		 * @return bool
 		 */
-		protected function process_form()
+		public function process_form()
 		{
-			global $uploads_dir;
-
-			// No data? no action?
-			if (empty($_POST) && !isset($_POST['action']) && $_POST['action'] == 'wicket-ac-profile-picture-form') {
+			if (is_admin()) {
 				return;
 			}
 
-			$form = $_POST['wicket-ac-profile-picture-form'];
+			// No data? no action?
+			if (!isset($_POST['action']) || $_POST['action'] !== 'wicket-ac-profile-picture-form') {
+				return;
+			}
+
+			$form = $_POST;
 
 			// Check nonce
 			if (!wp_verify_nonce(sanitize_text_field(wp_unslash($form['nonce'])), 'wicket-ac-profile-picture-form')) {
 				return false;
 			}
 
-			// Check if the file is set
-			if (empty($form['profile-image'])) {
+			// Check if the file is set and valid
+			if (!isset($_FILES['profile-image']) || $_FILES['profile-image']['error'] !== UPLOAD_ERR_OK) {
 				return false;
 			}
 
-			// Check if the file is too big
-			if (filesize(wp_unslash($form['profile-image']['tmp_name'])) > $this->pp_max_size) {
+			// Get the extension
+			$file_extension = pathinfo($_FILES['profile-image']['name'], PATHINFO_EXTENSION);
+
+			// Check if is a valid image
+			if (getimagesize($_FILES['profile-image']['tmp_name']) === false) {
 				return false;
 			}
 
-			// The file
-			$file    = wp_unslash($form['profile-image']);
+			// Check if the file size is too big. pp_max_size is in MB
+			if ($_FILES['profile-image']['size'] > $this->pp_max_size * 1024 * 1024) { // Convert MB to bytes
+				return false;
+			}
 
 			// User ID
 			$user_id = sanitize_text_field(wp_unslash($form['user_id']));
 
 			// Remove any existing file on wicket-profile-pictures/{user_id}.{extension}
-			$file_path   = $uploads_dir['basedir'] . '/wicket-profile-pictures/' . $user_id . '.' . $file['extension'];
+			$file_path   = $this->uploads_dir['basedir'] . '/wicket-profile-pictures/' . $user_id . '.' . $file_extension;
 
 			// Delete the file if it exists
-			if (file_exists($file_path)) {
-				wp_delete_file($file_path);
+			foreach ($this->pp_extensions as $ext) {
+				$other_file_path = $this->uploads_dir['basedir'] . '/wicket-profile-pictures/' . $user_id . '.' . $ext;
+
+				if (file_exists($other_file_path)) {
+					wp_delete_file($other_file_path);
+				}
 			}
 
 			// No matter whats the file name, rename it to {user_id}.{extension}
-			$file['name'] = $user_id . '.' . $file['extension'];
+			$_FILES['profile-image']['name']      = $user_id . '.' . $file_extension;
+			$_FILES['profile-image']['full_path'] = $user_id . '.' . $file_extension;
 
 			// Create subfolder if it doesn't exist
-			if (!file_exists($uploads_dir['basedir'] . '/wicket-profile-pictures')) {
-				wp_mkdir_p($uploads_dir['basedir'] . '/wicket-profile-pictures');
+			if (!file_exists($this->uploads_dir['basedir'] . '/wicket-profile-pictures')) {
+				wp_mkdir_p($this->uploads_dir['basedir'] . '/wicket-profile-pictures');
 			}
 
-			// Include WordPress file handling functions
-			require_once(ABSPATH . 'wp-admin/includes/file.php');
-			require_once(ABSPATH . 'wp-admin/includes/image.php');
-			require_once(ABSPATH . 'wp-admin/includes/media.php');
-
-			// Temporary change uploads path
-			add_filter('upload_dir', [$this, 'change_uploads_directory']);
-
-			// Move the file to the uploads directory
-			$movefile = wp_handle_upload($file, ['test_form' => false]);
+			// Move the file to the uploads directory, move_uploaded_file
+			$movefile = move_uploaded_file($_FILES['profile-image']['tmp_name'], $file_path);
 
 			// Check for errors
-			if (!$movefile || is_wp_error($movefile)) {
+			if (!$movefile) {
 				return false;
 			}
-
-			// Check if the file is valid jpg or png
-			if (!wp_check_filetype($movefile['file'], $movefile['type'])) {
-				return false;
-			}
-
-			// Return uploads path to normal
-			remove_filter('upload_dir', [$this, 'change_uploads_directory']);
 
 			return true;
 		}
-
-		/**
-		 * Change uploads directory
-		 *
-		 * @return void
-		 */
-		protected function change_uploads_directory($dir)
-		{
-			return [
-				'path'     => $this->pp_uploads_path,
-				'url'      => $this->pp_uploads_url,
-				'subdir'   => $this->pp_uploads_subdir,
-			] + $dir;
-		}
-	}
+	} // end Wicket_Acc_Profile_Picture class
 }
 
 /**
