a4f6ca89d706fadd42f5742c952b587bc0d422ac
Esteban <esteban@attitude.cl>
2024-07-30 10:56:43 -0400

    formatting

--- templates-wicket/front/dashboard.php
+++ templates-wicket/front/dashboard.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * My Account Dashboard
  *
@@ -17,56 +18,56 @@
  * @version 4.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
 ?>
 
-<?php 
+<?php
 
-	$wicket_acc_custom_dashboard_id = get_option( 'wicket_acc_set_ep_custom_dashboard' );
+$wicket_acc_custom_dashboard_id = get_option('wicket_acc_set_ep_custom_dashboard');
 
-	// if WPML is installed, get the translated AC landing page if it exists 
-	if ( defined( 'ICL_SITEPRESS_VERSION' ) ) {
-		$type = apply_filters( 'wpml_element_type', get_post_type( 	$wicket_acc_custom_dashboard_id ) );
-		$trid = apply_filters( 'wpml_element_trid', false, 	$wicket_acc_custom_dashboard_id, $type );
-		$translations = apply_filters( 'wpml_get_element_translations', array(), $trid, $type );		
-		if (isset($translations[ICL_LANGUAGE_CODE])) {
-			$wicket_acc_custom_dashboard_id = $translations[ICL_LANGUAGE_CODE]->element_id;
-		}
-	 }
-
-	if ($wicket_acc_custom_dashboard_id) {
-		$content_post = get_post($wicket_acc_custom_dashboard_id);
-		$content = $content_post->post_content;
-		$content = apply_filters('the_content', $content);
-		$content = str_replace(']]>', ']]&gt;', $content);
-		echo $content;
+// if WPML is installed, get the translated AC landing page if it exists
+if (defined('ICL_SITEPRESS_VERSION')) {
+	$type = apply_filters('wpml_element_type', get_post_type($wicket_acc_custom_dashboard_id));
+	$trid = apply_filters('wpml_element_trid', false, 	$wicket_acc_custom_dashboard_id, $type);
+	$translations = apply_filters('wpml_get_element_translations', array(), $trid, $type);
+	if (isset($translations[ICL_LANGUAGE_CODE])) {
+		$wicket_acc_custom_dashboard_id = $translations[ICL_LANGUAGE_CODE]->element_id;
 	}
+}
+
+if ($wicket_acc_custom_dashboard_id) {
+	$content_post = get_post($wicket_acc_custom_dashboard_id);
+	$content = $content_post->post_content;
+	$content = apply_filters('the_content', $content);
+	$content = str_replace(']]>', ']]&gt;', $content);
+	echo $content;
+}
 
 ?>
 
 <?php
-	/**
-	 * My Account dashboard.
-	 *
-	 * @since 2.6.0
-	 */
-	do_action( 'woocommerce_account_dashboard' );
+/**
+ * My Account dashboard.
+ *
+ * @since 2.6.0
+ */
+do_action('woocommerce_account_dashboard');
 
-	/**
-	 * Deprecated woocommerce_before_my_account action.
-	 *
-	 * @deprecated 2.6.0
-	 */
-	do_action( 'woocommerce_before_my_account' );
+/**
+ * Deprecated woocommerce_before_my_account action.
+ *
+ * @deprecated 2.6.0
+ */
+do_action('woocommerce_before_my_account');
 
-	/**
-	 * Deprecated woocommerce_after_my_account action.
-	 *
-	 * @deprecated 2.6.0
-	 */
-	do_action( 'woocommerce_after_my_account' );
+/**
+ * Deprecated woocommerce_after_my_account action.
+ *
+ * @deprecated 2.6.0
+ */
+do_action('woocommerce_after_my_account');
 
 /* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
--- templates-wicket/front/navigation.php
+++ templates-wicket/front/navigation.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * My Account navigation
  *
@@ -15,73 +16,73 @@
  * @version 2.6.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
 global $wp;
 
-$nav_heading 			= wp_get_nav_menu_name( 'wicket-acc-nav' );
-$nav_heading_two 	= wp_get_nav_menu_name( 'wicket-acc-nav-two' );
-$nav_layout				= get_option( 'wicket_acc_set_ep_as_fld' );
-do_action( 'woocommerce_before_account_navigation' );
+$nav_heading 			= wp_get_nav_menu_name('wicket-acc-nav');
+$nav_heading_two 	= wp_get_nav_menu_name('wicket-acc-nav-two');
+$nav_layout				= get_option('wicket_acc_set_ep_as_fld');
+do_action('woocommerce_before_account_navigation');
 ?>
 
-<?php do_action( 'woocommerce_before_account_navigation_ul' ); ?>
+<?php do_action('woocommerce_before_account_navigation_ul'); ?>
 
 <?php if (has_nav_menu('wicket-acc-nav')) : ?>
 	<div class="hidden lg:block myaccount-nav">
 		<?php if ($nav_heading) :
-			$myaccount_page = get_option( 'woocommerce_myaccount_page_id' );
+			$myaccount_page = get_option('woocommerce_myaccount_page_id');
 		?>
-		<h2 class="myaccount-nav-heading"><a href="<?php echo get_permalink($myaccount_page); ?>"><?php echo $nav_heading; ?></a></h2>
+			<h2 class="myaccount-nav-heading"><a href="<?php echo get_permalink($myaccount_page); ?>"><?php echo $nav_heading; ?></a></h2>
 		<?php endif; ?>
+		<?php
+		wp_nav_menu(array(
+			'container' => false,
+			'theme_location' => 'wicket-acc-nav',
+			'depth' => 3,
+			'menu_id' => 'wicket-acc-menu',
+			'menu_class' => 'wicket-acc-menu',
+			'walker' => new wicket_acc_menu_walker()
+		));
+		?>
+		<?php if (has_nav_menu('wicket-acc-nav-two')) : ?>
+			<?php if ($nav_heading_two) :
+				$myaccount_page = get_option('woocommerce_myaccount_page_id');
+			?>
+				<h2 class="myaccount-nav-heading"><a href="<?php echo get_permalink($myaccount_page); ?>"><?php echo $nav_heading_two; ?></a></h2>
+			<?php endif; ?>
 			<?php
-				wp_nav_menu(array(
-					'container' => false,
-					'theme_location' => 'wicket-acc-nav',
-					'depth' => 3,
-					'menu_id' => 'wicket-acc-menu',
-					'menu_class' => 'wicket-acc-menu',
-					'walker' => new wicket_acc_menu_walker()
-					));
+			wp_nav_menu(array(
+				'container' => false,
+				'theme_location' => 'wicket-acc-nav-two',
+				'depth' => 3,
+				'menu_id' => 'wicket-acc-menu-two',
+				'menu_class' => 'wicket-acc-menu-two',
+				'walker' => new wicket_acc_menu_walker()
+			));
 			?>
-      <?php if (has_nav_menu('wicket-acc-nav-two')) : ?>
-        <?php if ($nav_heading_two) :
-          $myaccount_page = get_option( 'woocommerce_myaccount_page_id' );
-        ?>
-        <h2 class="myaccount-nav-heading"><a href="<?php echo get_permalink($myaccount_page); ?>"><?php echo $nav_heading_two; ?></a></h2>
-        <?php endif; ?>
-        <?php
-				wp_nav_menu(array(
-					'container' => false,
-					'theme_location' => 'wicket-acc-nav-two',
-					'depth' => 3,
-					'menu_id' => 'wicket-acc-menu-two',
-					'menu_class' => 'wicket-acc-menu-two',
-					'walker' => new wicket_acc_menu_walker()
-					));
-			?>
-     <?php endif; ?>
+		<?php endif; ?>
 	</div>
 
 	<div class="col-lg-4 lg:hidden myaccount-nav myaccount-nav-mobile">
 		<div id="dropdown-my-account-menu" class="dropdown__content dropdown__content--nav" aria-labelledby="dropdown-control-my-account-menu" aria-expanded="false" role="region" style="display:none">
 			<?php
-				wp_nav_menu(array(
-					'container' => false,
-					'theme_location' => 'wicket-acc-nav',
-					'depth' => 3,
-					'menu_id' => 'wicket-acc-menu-mobile',
-					'menu_class' => 'wicket-acc-menu-mobile',
-					'walker' => new wicket_acc_menu_mobile_walker()
-					));
+			wp_nav_menu(array(
+				'container' => false,
+				'theme_location' => 'wicket-acc-nav',
+				'depth' => 3,
+				'menu_id' => 'wicket-acc-menu-mobile',
+				'menu_class' => 'wicket-acc-menu-mobile',
+				'walker' => new wicket_acc_menu_mobile_walker()
+			));
 			?>
-    </div>
+		</div>
 		<a href="#" id="dropdown-control-my-account-menu" class="dropdown__button dropdown__toggle dropdown__toggle--nav" aria-controls="dropdown-my-account-menu" aria-expanded="false"><?php echo $nav_heading; ?> <i class="fal fa-plus" aria-hidden="true"></i></a>
-    <?php if (has_nav_menu('wicket-acc-nav-two')) : ?>
-      <div id="dropdown-my-account-menu-two" class="dropdown__content dropdown__content--nav" aria-labelledby="dropdown-control-my-account-menu" aria-expanded="false" role="region" style="display:none">
-      <?php
+		<?php if (has_nav_menu('wicket-acc-nav-two')) : ?>
+			<div id="dropdown-my-account-menu-two" class="dropdown__content dropdown__content--nav" aria-labelledby="dropdown-control-my-account-menu" aria-expanded="false" role="region" style="display:none">
+				<?php
 				wp_nav_menu(array(
 					'container' => false,
 					'theme_location' => 'wicket-acc-nav-two',
@@ -89,12 +90,12 @@ do_action( 'woocommerce_before_account_navigation' );
 					'menu_id' => 'wicket-acc-menu-mobile-two',
 					'menu_class' => 'wicket-acc-menu-mobile-two',
 					'walker' => new wicket_acc_menu_mobile_walker()
-					));
-			?>
-      </div>
-      <a href="#" id="dropdown-control-my-account-menu" class="dropdown__button dropdown__toggle dropdown__toggle--nav" aria-controls="dropdown-my-account-menu-two" aria-expanded="false"><?php echo $nav_heading_two; ?> <i class="fal fa-plus" aria-hidden="true"></i></a>
-    <?php endif; ?>
-  </div>
+				));
+				?>
+			</div>
+			<a href="#" id="dropdown-control-my-account-menu" class="dropdown__button dropdown__toggle dropdown__toggle--nav" aria-controls="dropdown-my-account-menu-two" aria-expanded="false"><?php echo $nav_heading_two; ?> <i class="fal fa-plus" aria-hidden="true"></i></a>
+		<?php endif; ?>
+	</div>
 <?php endif; ?>
 
-<?php do_action( 'woocommerce_after_account_navigation' ); ?>
+<?php do_action('woocommerce_after_account_navigation'); ?>
