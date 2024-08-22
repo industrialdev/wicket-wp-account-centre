6647d76589da513e0c78269c08c3855ac569430b
Esteban <esteban@attitude.cl>
2024-08-22 11:57:53 -0400

    fixes template override

--- classes/class-wicket-acc-blocks.php
+++ classes/class-wicket-acc-blocks.php
@@ -181,7 +181,7 @@ class Blocks extends WicketAcc
 		$template_name = sanitize_title($template_name);
 
 		// Assume template is in child theme
-		$template_path = WICKET_ACC_USER_TEMPLATE_PATH 	. $template_name . '.php';
+		$template_path = WICKET_ACC_USER_TEMPLATE_PATH 	. 'blocks/' . WICKET_ACC_SLUG . '/' . $template_name . '.php';
 
 		if (!file_exists($template_path)) {
 			// If not found, check if template is in plugin folder
