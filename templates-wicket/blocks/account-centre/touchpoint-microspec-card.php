ead192f5edbf27dbc97a3971a80426e44a2857a3
Esteban <esteban@attitude.cl>
2024-08-27 16:58:22 -0400

    fixes location empty (but with commas)

--- templates-wicket/blocks/account-centre/touchpoint-microspec-card.php
+++ templates-wicket/blocks/account-centre/touchpoint-microspec-card.php
@@ -13,6 +13,17 @@ defined('ABSPATH') || exit;
 
 $tp         = $args['tp'];
 $event_data = urlencode(base64_encode(json_encode($tp)));
+
+if ($tp['attributes']['data']['location']) {
+	$location = $tp['attributes']['data']['location'];
+
+	// Check if location contains NO letters. If so, set to false
+	if (!preg_match('/[a-zA-Z]/', $location)) {
+		$location = false;
+	}
+} else {
+	$location = false;
+}
 ?>
 <div class="event-card my-4 p-4 border border-gray-200 rounded-md shadow-md" data-uuid="<?php echo $tp['id']; ?>">
 	<a href="?event-id=<?php echo $tp['id']; ?>&event-data=<?php echo $event_data; ?>" class="event-card-link">
@@ -25,8 +36,10 @@ $event_data = urlencode(base64_encode(json_encode($tp)));
 		<p class="text-sm mb-2 event-date">
 			<?php echo date('M', strtotime($tp['attributes']['data']['StartDate'])) . '-' . date('j', strtotime($tp['attributes']['data']['StartDate'])) . '-' . date('Y', strtotime($tp['attributes']['data']['StartDate'])) . ' | ' . date('g:i a', strtotime($tp['attributes']['data']['StartDate'])) . ' - ' . date('g:i a', strtotime($tp['attributes']['data']['EndDate'])); ?>
 		</p>
-		<p class="text-sm event-location hidden">
-			<strong><?php esc_attr_e('Location:', 'wicket-acc'); ?></strong>
-		</p>
+		<?php if ($location) : ?>
+			<p class="text-sm event-location">
+				<strong><?php esc_attr_e('Location:', 'wicket-acc'); ?></strong>
+			</p>
+		<?php endif; ?>
 	</a>
 </div>
--- templates-wicket/blocks/account-centre/touchpoint-tec-card.php
+++ templates-wicket/blocks/account-centre/touchpoint-tec-card.php
@@ -13,6 +13,17 @@ defined('ABSPATH') || exit;
 
 $tp         = $args['tp'];
 $event_data = urlencode(base64_encode(json_encode($tp)));
+
+if ($tp['attributes']['data']['location']) {
+	$location = $tp['attributes']['data']['location'];
+
+	// Check if location contains NO letters. If so, set to false
+	if (!preg_match('/[a-zA-Z]/', $location)) {
+		$location = false;
+	}
+} else {
+	$location = false;
+}
 ?>
 <div class="event-card my-4 p-4 border border-gray-200 rounded-md shadow-md" data-uuid="<?php echo $tp['id']; ?>">
 	<a href="<?php $tp['attributes']['data']['url']; ?>" class="event-card-link">
@@ -26,8 +37,10 @@ $event_data = urlencode(base64_encode(json_encode($tp)));
 		<p class="text-sm mb-2 event-date">
 			<?php echo date('M', strtotime($tp['attributes']['data']['start_date'])) . '-' . date('j', strtotime($tp['attributes']['data']['start_date'])) . '-' . date('Y', strtotime($tp['attributes']['data']['start_date'])) . ' | ' . date('g:i a', strtotime($tp['attributes']['data']['start_date'])) . ' - ' . date('g:i a', strtotime($tp['attributes']['data']['end_date'])); ?>
 		</p>
-		<p class="text-sm event-location hidden">
-			<strong><?php esc_attr_e('Location:', 'wicket-acc'); ?></strong>
-		</p>
+		<?php if ($location) : ?>
+			<p class="text-sm event-location">
+				<strong><?php esc_attr_e('Location:', 'wicket-acc'); ?></strong>
+			</p>
+		<?php endif; ?>
 	</a>
 </div>
