f1d41aa1bb15f099672991031146d79d7abb3be0
Esteban <esteban@attitude.cl>
2024-10-14 17:05:40 -0300

    PHP CS Fixer locally
    On project

--- .ci/install-hooks.sh
+++ .ci/install-hooks.sh
@@ -3,6 +3,18 @@
 # Get the directory of the script
 HOOKS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Check if there isn't a .git directory
+if [ ! -d "$HOOKS_DIR/../.git" ]; then
+    echo "No .git directory found. Skipping installation."
+    exit 0
+fi
+
+# Check if a git hook already exists
+if [ -f "$HOOKS_DIR/../.git/hooks/pre-commit" ]; then
+    echo "Git hook already exists. Skipping installation."
+    exit 0
+fi
+
 # Copy the hook to the .git/hooks/ directory
 cp "$HOOKS_DIR/pre-commit-hook" "$HOOKS_DIR/../.git/hooks/pre-commit"
 chmod +x "$HOOKS_DIR/../.git/hooks/pre-commit"
--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,23 +1,23 @@
 <?php
+
 /**
  * Wicket's PHP CS Fixer Configuration
+ *
  * @link https://github.com/PHP-CS-Fixer/PHP-CS-Fixer
  * @link https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/master/doc/config.rst
  */
 
-$finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
+$finder = PhpCsFixer\Finder::create()
     ->exclude([
-        'node_modules',
-        'vendor',
         '.ci',
-        'src',
-        'dist',
+        'node_modules',
+    'vendor',
         'languages',
     ])
     ->notName([
         'composer.lock',
-    ]);
+    ])
+    ->in(__DIR__);
 
 return (new PhpCsFixer\Config())
     ->setRules([
@@ -27,4 +27,5 @@ return (new PhpCsFixer\Config())
         'array_syntax'      => ['syntax' => 'short'],   // Enforce short array syntax
         'no_unused_imports' => true,                    // Remove unused imports
     ])
-    ->setFinder($finder);
+    ->setFinder($finder)
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect());
