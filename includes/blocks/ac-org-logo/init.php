bf6f7e277c7e33d420ae7316b8ff4ddb5c011fe2
Esteban <esteban@attitude.cl>
2024-12-13 16:14:35 -0300

    child org compatibility

--- includes/blocks/ac-org-logo/init.php
+++ includes/blocks/ac-org-logo/init.php
@@ -49,8 +49,8 @@ class init extends Blocks
 
         $person = wicket_current_person();
         $org_ids = [];
-        // figure out orgs I should see
-        // this association to the org is set on each role. The actual role types we look at might change depending on the project
+
+        // Figure out orgs I should see this association to the org is set on each role. The actual role types we look at might change depending on the project
         foreach ($person->included() as $person_included) {
             if (isset($person_included['attributes']['name'])) {
                 if ($person_included['type'] == 'roles' && (stristr($person_included['attributes']['name'], 'org_editor'))) {
@@ -61,8 +61,8 @@ class init extends Blocks
             }
         }
 
-        // If the org_id is not in the org_ids array, return
-        if (!in_array($org_id, $org_ids, true)) {
+        // If the org_id is not in the org_ids array, return. But let user pass if a child org is defined
+        if (!in_array($org_id, $org_ids, true) && empty($child_org_id)) {
             return;
         }
 
@@ -81,8 +81,8 @@ class init extends Blocks
         $process_form = $this->process_form();
         $remove_form = $this->remove_form();
 
-        $org_id = (isset($_GET['org_id'])) ? $_GET['org_id'] : '';
-        $child_org_id = (isset($_GET['child_org_id'])) ? $_GET['child_org_id'] : '';
+        $org_id = $_GET['org_id'] ?? '';
+        $child_org_id = $_GET['child_org_id'] ?? '';
 
         // Child organization compatibility
         if (!empty($child_org_id)) {
