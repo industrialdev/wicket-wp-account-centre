11cf07b31607311828a2d02cb8c83c6ad95eb979
Esteban <esteban@attitude.cl>
2025-07-22 12:55:21 -0400

    Fix legacy non-wicket theme issues

--- assets/js/wicket-acc-legacy.js
+++ assets/js/wicket-acc-legacy.js
@@ -1,9 +1,7 @@
 jQuery(document).ready(function ($) {
-
     'use strict';
 
     $('li.group_endpoint').each(function () {
-
         if ($(this).find('.nav-selected').length > 0) {
             $(this).addClass('nav-selected');
         }
@@ -20,7 +18,6 @@ jQuery(document).ready(function ($) {
                 $(this).parent().addClass('nav-selected');
             }
         }
-
     });
 
     $("#wicket_acc_file_upload").on('change', function () {
@@ -31,8 +28,9 @@ jQuery(document).ready(function ($) {
         $("#wicket_acc_file_upload").click();
     });
 
-    $(document).on('click', ".dropdown__toggle--menu", function (event) {
+    $('.wicket-acc-menu, .wicket-acc-menu-mobile, .myaccount-nav').on('click', ".dropdown__toggle--menu", function (event) {
         event.preventDefault();
+        event.stopImmediatePropagation();
         $(this).toggleClass("open");
         var targetContent = '#' + $(this).attr('aria-controls');
         if ($(this).hasClass("open")) {
@@ -58,19 +56,4 @@ jQuery(document).ready(function ($) {
             $(dropdownButton).addClass('open').attr("aria-expanded", "true");
         }
     }
-
-    $(document).on('click', ".dropdown__toggle", function (event) {
-        event.preventDefault();
-        $(this).toggleClass("open");
-        var targetContent = '#' + $(this).attr('aria-controls');
-        if ($(this).hasClass("open")) {
-            $(targetContent).slideDown('fast').attr("aria-expanded", "true");
-            $(this).attr("aria-expanded", "true");
-        } else {
-            $(targetContent).slideUp('fast').attr("aria-expanded", "false");
-            $(this).attr("aria-expanded", "false");
-        }
-        $(this).focus();
-    });
-
 });
