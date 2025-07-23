0d012d0a8a06a9eacb69f289319c737470faf58e
aykan <aykanburcak@gmail.com>
2025-07-23 17:36:23 +0300

    Fixing data type error on maple touchpoint block

--- includes/blocks/ac-touchpoint-maple/init.php
+++ includes/blocks/ac-touchpoint-maple/init.php
@@ -318,7 +318,7 @@ class init extends Blocks
             return $touchpoints;
         }
 
-        $touchpoints = $touchpoints['data'];
+        $touchpoints = $touchpoints['data'] ?? [];
 
         $filtered_touchpoints = array_filter($touchpoints, function ($touchpoint) use ($action) {
             if (isset($touchpoint['attributes']['action'])) {
